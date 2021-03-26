5b700ff51ce079bd16af3fcf7c0d9f7d52b933e0
Tosin-JD <oluwatosinjosephdurodola@gmail.com>
2021-03-26 18:49:14 +0100

    Commit

--- lyrics_app/settings.py
+++ lyrics_app/settings.py
@@ -32,6 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
                     'https://coollyrics.herokuapp.com/',
                     'coollyrics.herokuapp.com/',
+                    'coollyrics.herokuapp.com',
                     '127.0.0.1',
                 ]
 
